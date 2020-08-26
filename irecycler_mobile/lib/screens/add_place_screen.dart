37d2b914a62bc4152aa399ae4734f00878060f1d
Pablo Barría <27pablo08@gmail.com>
2020-08-26 09:59:24 -0400

    Layout inicial de screen agregar lugar

---
+++ irecycler_mobile/lib/screens/add_place_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  static const routeName = '/add-place';
+
+  @override
+  _AddPlaceScreenState createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
