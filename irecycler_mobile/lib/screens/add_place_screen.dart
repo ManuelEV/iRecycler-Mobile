44c96ecb019bb1818b7cd11ea88f7e371b85c5e1
Pablo Barría <27pablo08@gmail.com>
2020-09-05 18:23:04 -0400

    Agregar lugar layout

--- irecycler_mobile/lib/screens/add_place_screen.dart
+++ irecycler_mobile/lib/screens/add_place_screen.dart
@@ -13,42 +13,75 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Añade un nuevo punto de reciclaje'),
-        backgroundColor: Colors.indigo[400],
       ),
       body: Center(
-          child: Column(
-        children: [
-          Container(
-              width: 200,
-              height: 200,
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                      image: NetworkImage(
-                          'https://media-cdn.tripadvisor.com/media/photo-s/0b/71/10/23/hachiko.jpg'),
-                      fit: BoxFit.fill))),
-          RaisedButton.icon(
-              onPressed: null,
-              icon: Icon(Icons.add_a_photo),
-              label: Text('Añadir imagen del lugar')),
-          ConstrainedBox(
-            constraints: BoxConstraints.tight(Size(200, 50)),
-            child: TextFormField(
-              decoration:
-                  InputDecoration(labelText: 'Nombre del punto de reciclaje'),
+        child: Column(
+          children: <Widget>[
+            _placeImage(),
+            addPlaceImage(),
+            _addPlacementName(),
+            _coordinates_btn(),
+            Divider(
+              color: Colors.grey,
+              thickness: 2,
             ),
-          ),
-          Text('Ingreso de datos del usuario'),
-          RaisedButton.icon(
-            onPressed: null,
-            icon: Icon(
-              Icons.location_on,
-              color: Colors.red[400],
-            ),
-            label: Text('Utilizar tu ubicación actual'),
-          ),
-        ],
-      )),
+            _keywords(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _placeImage() {
+    return Container(
+      width: 200,
+      height: 200,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+            image: NetworkImage(
+                'https://media-cdn.tripadvisor.com/media/photo-s/0b/71/10/23/hachiko.jpg'),
+            fit: BoxFit.fill),
+      ),
+    );
+  }
+
+  Widget addPlaceImage() {
+    return Container(
+      alignment: Alignment.center,
+      child: RaisedButton.icon(
+        onPressed: null,
+        icon: Icon(Icons.add_a_photo),
+        label: Text('Picture'),
+      ),
+    );
+  }
+
+  Widget _coordinates_btn() {
+    return RaisedButton(
+      child: Text('Coordenadas'),
+      onPressed: () {},
+    );
+  }
+
+  Widget _addPlacementName() {
+    return Row(
+      children: [
+        Text('Nombre del lugar'),
+        IconButton(
+          visualDensity: VisualDensity.comfortable,
+          icon: Icon(Icons.edit),
+          tooltip: 'Nombre lugar',
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+
+  Widget _keywords() {
+    return Container(
+      child: Text('Keywords'),
     );
   }
 }
