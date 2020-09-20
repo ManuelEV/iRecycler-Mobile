e2e8c95779b30c967005c50824da7ba10b632798
Pablo Barría <27pablo08@gmail.com>
2020-09-20 16:23:38 -0300

    Se crea una página base de gráficos de los puntos del usuario

---
+++ irecycler_mobile/lib/screens/my_points_screen.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:irecycler_mobile/models/point.dart';
+import 'dart:io';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:flutter/material.dart';
+import 'package:irecycler_mobile/widgets/graph_widget.dart';
+
+class MyPointsScreen extends StatefulWidget {
+  static const routeName = '/my_points';
+  @override
+  _MyPointsScreenState createState() => _MyPointsScreenState();
+}
+
+class _MyPointsScreenState extends State<MyPointsScreen> {
+  List<charts.Series<LinearSales, String>> _createSampleData() {
+    final data = [
+      new LinearSales("Lleno", 75),
+      new LinearSales("Vacio", 25),
+    ];
+
+    return [
+      new charts.Series<LinearSales, String>(
+        id: 'Sales',
+        domainFn: (LinearSales sales, _) => sales.estado,
+        measureFn: (LinearSales sales, _) => sales.sales,
+        data: data,
+        // Set a label accessor to control the text of the arc label.
+        labelAccessorFn: (LinearSales row, _) => '${row.estado}: ${row.sales}',
+      )
+    ];
+  }
+
+  final List<Place> points = [
+    Place(
+      id: '1',
+      title: 'Punto 1',
+      description: 'adsdasdasd',
+      location: PlaceLocation(longitude: 3.123, latitude: 22.33),
+      filled: 50,
+      image: File('asdasd'),
+    ),
+    Place(
+      id: '2',
+      title: 'Punto 2',
+      filled: 75,
+      description: 'adsdasdasd',
+      location: PlaceLocation(longitude: 3.123, latitude: 22.33),
+      image: File('asdasd'),
+    ),
+    Place(
+      id: '3',
+      title: 'Punto 3',
+      filled: 89,
+      description: 'adsdasdasd',
+      location: PlaceLocation(longitude: 3.123, latitude: 22.33),
+      image: File('asdasd'),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          DropdownButton(
+            items: <String>['1 Mes', '6 Meses', '1 Año', '5 Años']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: (String newValue) {
+              setState(() {});
+            },
+          )
+        ],
+        title: Text('Mis puntos de reciclaje'),
+      ),
+      body: ListView.builder(
+        itemCount: this.points.length,
+        itemBuilder: (context, index) {
+          return Container(
+            height: 500,
+            child: Card(
+              child: Column(
+                children: <Widget>[
+                  Text(this.points[index].title),
+                  Container(
+                    width: 300,
+                    height: 400,
+                    child: DonutAutoLabelChart(
+                      this._createSampleData(),
+                      animate: true,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+/// Sample linear data type.
+class LinearSales {
+  final String estado;
+  final int sales;
+
+  LinearSales(this.estado, this.sales);
+}
