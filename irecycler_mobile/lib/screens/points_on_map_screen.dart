8bf75a5350356a4707783d46cacce7aba5a60a6f
Pablo Barría <27pablo08@gmail.com>
2020-10-07 00:32:19 -0300

    Cargando marcadores mapas

--- irecycler_mobile/lib/screens/points_on_map_screen.dart
+++ irecycler_mobile/lib/screens/points_on_map_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:irecycler_mobile/services/auth.dart';
 import 'package:irecycler_mobile/models/point.dart';
+import 'package:irecycler_mobile/services/points_service.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'drawer_page.dart';
 
@@ -18,7 +19,22 @@ class PointOnMapScreen extends StatefulWidget {
 }
 
 class _PointOnMapScreenState extends State<PointOnMapScreen> {
-  Map<Marker, MarkerId> marker = <Marker, MarkerId>{};
+  FirestoreService fS = FirestoreService();
+  List<Place> _points;
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+
+  Future fetchPoints() async {
+    var pointsResults = await fS.getPlacesOnceOff();
+    print(pointsResults);
+    if (pointsResults is List<Place>) {
+      _points = pointsResults;
+      //notifyListeners();
+    } else {
+      print('error');
+    }
+  }
+
+  populateMap() {}
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +42,11 @@ class _PointOnMapScreenState extends State<PointOnMapScreen> {
       appBar: AppBar(
         title: Text('Puntos en el mapa'),
         elevation: 0.0,
+        actions: [
+          FloatingActionButton(
+            onPressed: () => {fetchPoints(), print(_points)},
+          ),
+        ],
       ),
       drawer: DrawerPage(),
       body: GoogleMap(
@@ -35,8 +56,9 @@ class _PointOnMapScreenState extends State<PointOnMapScreen> {
           zoom: 13,
         ),
         //myLocationButtonEnabled: true,
-
         myLocationEnabled: true,
+        markers: Set<Marker>.of(markers.values),
+        onMapCreated: (GoogleMapController controller) {},
         onTap: null,
       ),
     );
